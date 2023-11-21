d33f9104a308db59573bbcf8d868236de4c4bbe3
Ulysse248 <114987648+Ulysse248@users.noreply.github.com>
2023-11-21 16:44:23 +0100

    Add files via upload
    Add the main file

---
+++ main.py
@@ -0,0 +1,114 @@
+""" The main function of rPPG deep learning pipeline."""
+
+import argparse
+import random
+import time
+
+import numpy as np
+import torch
+from config import get_config
+from dataset import data_loader
+from dataset.data_loader import UlysseLoader
+from testoverlap import output
+from neural_methods import trainer
+from torch.utils.data import DataLoader
+
+import matplotlib.pyplot as plt
+import cv2
+import glob
+import os
+
+RANDOM_SEED = 100
+torch.manual_seed(RANDOM_SEED)
+torch.cuda.manual_seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+random.seed(RANDOM_SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+# Create a general generator for use with the validation dataloader,
+# the test dataloader, and the unsupervised dataloader
+general_generator = torch.Generator()
+general_generator.manual_seed(RANDOM_SEED)
+# Create a training generator to isolate the train dataloader from
+# other dataloaders and better control non-deterministic behavior
+train_generator = torch.Generator()
+train_generator.manual_seed(RANDOM_SEED)
+
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2 ** 32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+
+def add_args(parser):
+    """Adds arguments for parser."""
+    parser.add_argument('--config_file', required=False,
+                        default="configs/train_configs/PURE_PURE_UBFC-rPPG_TSCAN_BASIC.yaml", type=str, help="The name of the model.")
+    '''Neural Method Sample YAML LIST:
+        UlysseConfig.yaml
+    '''
+    return parser
+
+def test(config, data_loader_dict):
+    """Tests the model."""
+    if config.MODEL.NAME == "EfficientPhys":
+        model_trainer = trainer.EfficientPhysTrainer.EfficientPhysTrainer(config, data_loader_dict)
+    else:
+        raise ValueError('Your Model is Not Supported  Yet!')
+    signal_data = model_trainer.test(data_loader_dict)
+    video_path = glob.glob(config.TEST.DATA.DATA_PATH + '*.MOV')[0]
+    output(signal_data, video_path)
+
+if __name__ == "__main__":
+    # parse arguments.
+    parser = argparse.ArgumentParser()
+    parser = add_args(parser)
+    parser = trainer.BaseTrainer.BaseTrainer.add_trainer_args(parser)
+    parser = data_loader.BaseLoader.BaseLoader.add_data_loader_args(parser)
+    args = parser.parse_args()
+
+    # configurations.
+    config = get_config(args)
+    print('Configuration:')
+    print(config, end='\n\n')
+
+    data_loader_dict = dict() # dictionary of data loaders 
+
+    if config.TOOLBOX_MODE == "only_test":
+        # test_loader
+        if config.TEST.DATA.DATASET == "Ulysse":
+            test_loader = UlysseLoader.UlysseLoader
+        else:
+            raise ValueError("Unsupported dataset! Currently supporting Ulysse \
+                                (custom dataset for just 1 video).")
+        
+        if config.TOOLBOX_MODE == "train_and_test" and config.TEST.USE_LAST_EPOCH:
+            print("Testing uses last epoch, validation dataset is not required.", end='\n\n')   
+
+        # Create and initialize the test dataloader given the correct toolbox mode,
+        # a supported dataset name, and a valid dataset path
+        if config.TEST.DATA.DATASET and config.TEST.DATA.DATA_PATH:
+            test_data = test_loader(
+                name="test",
+                data_path=config.TEST.DATA.DATA_PATH,
+                config_data=config.TEST.DATA)
+            data_loader_dict["test"] = DataLoader(
+                dataset=test_data,
+                num_workers=8,
+                batch_size=config.INFERENCE.BATCH_SIZE,
+                shuffle=False,
+                worker_init_fn=seed_worker,
+                generator=general_generator
+            )
+        else:
+            data_loader_dict['test'] = None
+
+    else:
+        raise ValueError("Unsupported toolbox_mode! Currently support train_and_test or only_test or unsupervised_method.")
+
+
+    if config.TOOLBOX_MODE == "only_test":
+        test(config, data_loader_dict)
+    else:
+        print("TOOLBOX_MODE only support only_test !", end='\n\n')
